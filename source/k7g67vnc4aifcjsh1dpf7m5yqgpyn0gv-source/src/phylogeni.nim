3fdde1c05707e6bb8a0040b14e3d7d2855ccb17c
hugosenari <hugosenari@gmail.com>
2022-09-03 19:16:59 -0300

    chore: re index phylogeni

---
+++ source/k7g67vnc4aifcjsh1dpf7m5yqgpyn0gv-source/src/phylogeni.nim
@@ -0,0 +1,9 @@
+import ./phylogeni/tree,
+       ./phylogeni/io/parseNewick,
+       ./phylogeni/io/writeNewick,
+       ./phylogeni/simulate
+
+export tree,
+       parseNewick,
+       writeNewick,
+       simulate
