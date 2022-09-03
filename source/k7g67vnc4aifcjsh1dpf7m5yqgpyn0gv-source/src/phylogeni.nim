c4b2f008e76f759b03ccfc1d92795eebd8f66d07
hugosenari <hugosenari@gmail.com>
2022-09-03 19:50:42 -0300

    feat(nimpkgs): update lock

--- source/k7g67vnc4aifcjsh1dpf7m5yqgpyn0gv-source/src/phylogeni.nim
+++
@@ -1,9 +0,0 @@
-import ./phylogeni/tree,
-       ./phylogeni/io/parseNewick,
-       ./phylogeni/io/writeNewick,
-       ./phylogeni/simulate
-
-export tree,
-       parseNewick,
-       writeNewick,
-       simulate
